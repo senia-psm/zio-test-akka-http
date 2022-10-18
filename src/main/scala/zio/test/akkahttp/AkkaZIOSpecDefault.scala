9a0be403822df41810a05e0f418c957e1e0d2dc1
Dominik Dorn <dominik@dominikdorn.com>
2022-10-18 14:26:42 +0200

    adjustment to bootstrap
    In the current zio snapshot, bootstrap in ZIOSpecAbstract is a
    ```
    def bootstrap: ZLayer[Any, Any, Environment]
    ```

--- src/main/scala/zio/test/akkahttp/AkkaZIOSpecDefault.scala
+++ src/main/scala/zio/test/akkahttp/AkkaZIOSpecDefault.scala
@@ -5,7 +5,7 @@ import zio.test.{testEnvironment, Spec, TestEnvironment, ZIOSpec}
 
 trait AkkaZIOSpecDefault extends ZIOSpec[RouteTestEnvironment.TestEnvironment with TestEnvironment] with RouteTest {
 
-  override val bootstrap: ZLayer[Scope, Any, RouteTestEnvironment.TestEnvironment with TestEnvironment] =
+  override val bootstrap: ZLayer[Any, Any, RouteTestEnvironment.TestEnvironment with TestEnvironment] =
     testEnvironment >+> RouteTestEnvironment.environment
 
   def spec: Spec[RouteTestEnvironment.TestEnvironment with TestEnvironment with Scope, Any]
